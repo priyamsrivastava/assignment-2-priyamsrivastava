8369602db72b77e595d22e0e2c23fd7eb78c7ef1
priyamsrivastava <priyam.srivastava_cs18@gla.ac.in>
2020-03-28 20:57:07 +0530

    made student and nde class

--- src/problem3/node/Node.java
+++ src/problem3/node/Node.java
@@ -6,5 +6,33 @@
  */
 package problem3.node;
 
+
 public class Node {
+    private Student data;
+    private Node next;
+
+    public Node(Student d) {
+        data = d;
+        next = null;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
+    public Student getData() {
+        return data;
+    }
+
+    public void setData(Student data) {
+        this.data = data;
+    }
+
+    public int compareTo(Student data) {
+        return data.getRollNo() - this.data.getRollNo();
+    }
 }
