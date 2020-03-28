7b3bd54eadf1dfaa78b47f3b5836917aacf81632
priyamsrivastava <priyam.srivastava_cs18@gla.ac.in>
2020-03-28 21:08:33 +0530

    created the constructor

--- src/problem4/myqueue/MyQueue.java
+++ src/problem4/myqueue/MyQueue.java
@@ -11,4 +11,11 @@ public class MyQueue {
     private Node front, end, tmp;
     private int size;
 
+    public MyQueue() {
+        front = null;
+        end = null;
+        tmp = null;
+        size = 0;
+
+    }
 }
